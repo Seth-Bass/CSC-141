79b88646e4bd4a69cf201fdfc9948e33de271715
Seth <seth.bassett001@albright.edu>
2024-09-05 10:05:48 -0400

    Add files via upload

---
+++ 01_getting_started/01_01_python_org.py
@@ -0,0 +1,19 @@
+'''
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  
+
+There is no one real topic that really interest me.
+
+I have just never been that kind ofo person.
+
+I am more of a jack of all trades.
+
+I just want to learn as much as I can.
+
+That way I can have as much of an advantage as I can when I face problems.
+
+After all, there is nothing really wrong with learning as much as you can right?
+
+'''
